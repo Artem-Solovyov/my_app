921147290e266ea62075463d20e4b698b11062d9
Artem Solovyov <solovevartem892@gmail.com>
2022-08-07 16:34:45 +0300

    Flux, OOP, dispatch, creator + all files//2

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -8,13 +8,13 @@ const ProfileInfo = () => {
         <div>
           <img
               src="https://www.kultur-port.de/images/stories/blog/2022/Horizont_F_Pexelspixabay.jpg"
-              className={s.first_img}/>
+              className={s.first_img} alt='ava'/>
         </div>
         {/* avatar and description */}
         <div className={s.discription_block}>
           <img
               src="https://images.pexels.com/photos/1043473/pexels-photo-1043473.jpeg?auto=compress&cs=tinysrgb&w=600"
-              className={s.second_img}
+              className={s.second_img}  alt='ava'
           />
         </div>
       </div>
